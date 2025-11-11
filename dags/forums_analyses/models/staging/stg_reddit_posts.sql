5032f446cd3a9bee5eac26bdfe9564cbd40afe78
08Aristodemus24 <MichaelAveuc571@gmail.com>
2025-11-11 22:30:46 +0800

    update

---
+++ dags/forums_analyses/models/staging/stg_reddit_posts.sql
@@ -0,0 +1,26 @@
+{{ 
+    config(
+        materialized='incremental',
+        unique_key=['post_id']
+    )
+}}
+
+WITH reddit_posts AS (
+    SELECT
+        *,
+        
+        -- Get the last load time from the warehouse for incremental logic
+        CURRENT_TIMESTAMP() AS dbt_load_timestamp
+        -- Add more columns as needed
+    FROM {{ source('forums_data', 'raw_reddit_posts') }}
+)
+
+SELECT *
+FROM reddit_posts
+{% if is_incremental() %}
+WHERE
+    -- Process files that were created/uploaded to S3 *after* the last max timestamp
+    -- stored in your Snowflake destination table.
+    -- we use the comment/reply's created_at timestmap    
+    added_at > (SELECT MAX(added_at) FROM reddit_posts)
+{% endif %}
