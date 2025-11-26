370e7001ed34dcffe9c2372ae6db2f5975d14426
08Aristodemus24 <MichaelAveuc571@gmail.com>
2025-11-26 17:41:02 +0800

    update

--- dags/forums_analyses/models/intermediate/int_reddit_users.sql
+++ dags/forums_analyses/models/intermediate/int_reddit_users.sql
@@ -6,8 +6,8 @@
 
 SELECT
     DISTINCT
-    comment_author_name AS user_id,
-    comment_author_fullname AS username
+    comment_author_username AS username,
+    comment_author_id_full AS user_id
 FROM {{ ref('stg_reddit_posts_comments') }}
 WHERE comment_author_name IS NOT NULL
 
@@ -15,7 +15,7 @@ UNION BY NAME
 
 SELECT
     DISTINCT
-    post_name AS user_id,
-    post_author_name AS username
+    post_author_id_full AS user_id,
+    post_author_username AS username
 FROM {{ ref('stg_reddit_posts') }}
 WHERE post_name IS NOT NULL
