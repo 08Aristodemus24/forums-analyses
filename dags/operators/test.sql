56d5d096a92bfa7d38f2a391f6289ca43bf57c36
08Aristodemus24 <MichaelAveuc571@gmail.com>
2026-01-07 20:26:49 +0800

    update

---
+++ dags/operators/test.sql
@@ -0,0 +1,11 @@
+USE forums_analyses_db;
+USE forums_analyses_db.forums_analyses_bronze;
+
+
+SELECT
+    COUNT(*),
+    video_id,
+    comment_id
+FROM raw_youtube_videos_comments
+GROUP BY ALL
+HAVING COUNT(*) > 1;
