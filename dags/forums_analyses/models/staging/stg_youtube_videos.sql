2df3215130d83d5d5a2a2ea429408592ee169b8e
Larry Miguel Cueva <larrymiguelcueva@gmail.com>
2026-01-12 05:30:47 +0000

    commit form dbt

---
+++ dags/forums_analyses/models/staging/stg_youtube_videos.sql
@@ -0,0 +1,32 @@
+{{ 
+    config(
+        materialized='incremental',
+        unique_key=['video_id'],
+        on_schema_change='sync_all_columns',
+        incremental_strategy='merge'
+    )
+}}
+
+WITH youtube_videos AS (
+    video_id,
+    duration AS video_duration,
+    channel_id AS videos_channel_id,
+    channel_title AS channel_name,
+    video_title,
+    video_description,
+    video_tags,
+    comment_count,
+    favorite_count,
+    like_count,
+    view_count,
+    made_for_kids,
+    published_at AS video_created_at,
+    added_at
+)
+
+SELECT *
+FROM youtube_videos
+{% if is_incremental() %}
+WHERE
+    added_at > (SELECT MAX(added_at) FROM {{ this }})
+{% endif %}
