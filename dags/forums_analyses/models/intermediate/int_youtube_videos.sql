2dc847ced75577dfa588e3949f7bb7ace68c45b9
Larry Miguel Cueva <larrymiguelcueva@gmail.com>
2026-01-13 04:03:32 +0000

    update

---
+++ dags/forums_analyses/models/intermediate/int_youtube_videos.sql
@@ -0,0 +1,22 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
+
+SELECT
+    video_id,
+    video_duration,
+    videos_channel_id,
+    channel_name,
+    video_title,
+    video_description,
+    video_tags,
+    comment_count,
+    favorite_count,
+    like_count,
+    view_count,
+    made_for_kids,
+    video_created_at,
+    added_at
+FROM {{ref("stg_youtube_videos")}}
